80661329d22ed59098e398469d5e5375eb132886
Dom <dom.benatar@hotmail.com>
2023-10-27 14:42:35 +0100

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -7,11 +8,11 @@ function App() {
     <header>
       <h1>React router Dom</h1>
         <Link to="/">Home</Link>
-        <Link to="/barnicles"></Link>
+        <Link to="/team"></Link>
     </header>
     <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/barnicles" element={<Barnicles />} />
+    <Route path="/team" element={<Team />} />
     </Routes> 
 
     <footer><p>&copy;Created be Dom Benatar for Maia</p></footer>
