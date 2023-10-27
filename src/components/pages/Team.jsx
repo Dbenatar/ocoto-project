d4716b352533c19979820844a06e774d83c7f74f
Dom <dom.benatar@hotmail.com>
2023-10-27 15:29:55 +0100

    team bios added

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -3,14 +3,31 @@ export default function Team() {
     <>
     <h2>Octo crew</h2>
 
-    <div className="container">Captain Barnicles</div>
-    <div className="container">Kwazii</div>
-    <div className="container">Peso</div>
-    <div className="container">Shellington</div>
-    <div className="container">Dashi</div>
-    <div className="container">Inkling</div>
-    <div className="container">Tweek</div>
-    <div className="container">Tunip</div>
+    <div className="container"><h3>Captain Barnicles</h3>
+    <img src="" alt="" />
+    <p>Captain Barnacles is a brave polar bear extraordinaire and the leader of the Octonauts crew. He's always the first to rush in and help whenever there's a problem. Besides adventuring, Barnacles enjoys playing his accordion and writing in his captain's log.
+</p></div>
+    <div className="container"><h3>Kwazii</h3>
+    <img src="" alt="" />
+    <p>Kwazii is a daredevil orange kitten with a mysterious pirate past. He loves excitement and travelling to exotic places. His favourite hobbies include long baths, racing the Gup-B, and general swashbuckling.</p></div>
+    <div className="container"><h3>Peso</h3>
+    <img src="" alt="" />
+    <p>Peso is the medic for the team. He's an expert at bandaging and always carries his medical kit with him in case of emergencies. He's not too fond of scary things, but if a creature is hurt or in danger, he can be the bravest Octonaut of all!</p></div>
+    <div className="container"><h3>Shellington</h3>
+    <img src="" alt="" />
+    <p>Shellington is a nerdy sea otter scientist who loves doing field research and working in his lab. He's easily distracted by rare plants and animals, but his knowledge of the ocean is a big help in Octonaut missions.</p></div>
+    <div className="container"><h3>Dashi</h3>
+    <img src="" alt="" />
+    <p>Dashi is a smart dachshund / sausage dog who oversees operations in the Octopod HQ and launch bay. She monitors the computer systems and manages all ship traffic. She's also the Octonauts' official photographer and enjoys taking photos of undersea life.</p></div>
+    <div className="container"><h3>Professor Inkling</h3>
+    <img src="" alt="" />
+    <p>Professor Inkling is a brilliant, Dumbo octopus oceanographer. He founded the Octonauts with the intention of furthering underwater research and preservation. Because of his delicate, big brain, he prefers to help the team from his library in the Octopod.</p></div>
+    <div className="container"><h3>Tweek</h3>
+    <img src="" alt="" />
+    <p>Tweak is the engineer for the Octopod. She keeps everything shipshape below deck in the engine room and maintains all Octonaut vehicles: GUP-A to GUP-Z. Tweak loves working with machinery and building strange contraptions that sometimes work in unexpected ways.</p></div>
+    <div className="container"><h3>Tunip</h3>
+    <img src="" alt="" />
+    <p>Tunip is one of many Vegimals, a special underwater critter (part vegetable / part animal) who help out around the Octopod. They speak their own language that only Shellington can understand (sometimes!). Vegimals love to cook and bake: kelp cakes, kelp cookies, kelp soufflé...</p></div>
 
     </>
   )
