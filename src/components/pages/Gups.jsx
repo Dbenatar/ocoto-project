948b255b7291d665365e9c783beb945a9b550a56
Dom <dom.benatar@hotmail.com>
2023-12-13 13:04:54 +0000

    updates made to gup page, up until gup R

--- src/components/pages/Gups.jsx
+++ src/components/pages/Gups.jsx
@@ -1,34 +1,414 @@
 export default function Gups() {
-  return(
+  return (
     <>
-    <h2>Gups</h2>
-    <div className="container">Gup A</div>
-    <div className="container">Gup B</div>
-    <div className="container">Gup C</div>
-    <div className="container">Gup D</div>
-    <div className="container">Gup E</div>
-    <div className="container">Gup F</div>
-    <div className="container">Gup G</div>
-    <div className="container">Gup H</div>
-    <div className="container">Gup I</div>
-    <div className="container">Gup J</div>
-    <div className="container">Gup K</div>
-    <div className="container">Gup L</div>
-    <div className="container">Gup M</div>
-    <div className="container">Gup N</div>
-    <div className="container">Gup O</div>
-    <div className="container">Gup P</div>    
-    <div className="container">Gup Q</div>
-    <div className="container">Gup R</div>
-    <div className="container">Gup S</div>
-    <div className="container">Gup T</div>
-    <div className="container">Gup U</div>
-    <div className="container">Gup V</div>
-    <div className="container">Gup W</div>
-    <div className="container">Gup X</div>
-    <div className="container">Gup Y</div>
-    <div className="container">Gup Z</div>
-    
+      <h2>Gups</h2>
+      <div className="container">
+        <h3>Gup A</h3>
+        <img src="src/assets/images/Gup-A.webp" alt="An image of the Gup A" />
+        <p>
+          The Gup-A is a multi-purpose vehicle based on an anglerfish. Its speed
+          is moderate, but it has a turbo booster can make it go almost as fast
+          as the Gup-B
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>
+            Real-time navigation system, capable of tracking and predicting the
+            position of objects, crewmates, storms, ocean currents, and
+            geological hazards.
+          </li>
+          <li>Object shooter, intended for deploying bait.</li>
+          <li>
+            Drooping lamp from the top of the hull to distract or lure creatures
+            and light the path when the waters get dark.
+          </li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup B</h3>
+        <img src="src/assets/images/Gup-B.webp" alt="An image of the Gup B" />
+        <p>
+          The Gup-B is a shark-styled Gup with one seat. Known for its fast
+          speed and acceleration, it is Kwazii's favorite Gup. He mainly uses it
+          for almost all the missions the Octonauts go on, takes it out for solo
+          missions, or to just have fun driving it around, such as racing with
+          fast creature. It is also the fastest Gup of all, even faster than the
+          Gup-R.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>High-efficiency bubble engine for smooth and fast travel</li>
+          <li>Flying Fish Mode</li>
+          <li>
+            Overdrive engine for flank speed like the Gup-A (Turbo button)
+          </li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup C</h3>
+        <img src="src/assets/images/Gup-c.webp" alt="An image of the Gup C" />
+        <p>
+          The Gup-C is a big, blue Gup with a large towline. It is Shellington
+          and Dashi's favorite. The towline is for pulling Gups out of wedged
+          objects, for example, rocks. The window at the top is used for the
+          icebreaker attachment. With the exception of Professor Inkling Octopus
+          and the Vegimals, all the Octonauts have driven the Gup-C.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>
+            Reinforced body shell for smashing through large objects or thick
+            ice with a deployable ice-breaker to smash through tough chunks of
+            ice.
+          </li>
+          <li>Industrial grade tow-cable and anchor.</li>
+          <li>Turbulence resistant propeller.</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup D</h3>
+        <img src="src/assets/images/Gup-d.webp" alt="An image of the Gup D" />
+        <p>
+          The Gup-D is a small, multipurpose electric Gup with retractable arms
+          and legs. With its legs and arms out it looks like a crab, but with
+          its legs and arms in it looks like a manta ray.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Retractable claw arms and retractable legs</li>
+          <li>Strong metal claws</li>
+          <li>Carrot shooter</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup E</h3>
+        <img src="src/assets/images/Gup-e.webp" alt="An image of the Gup E" />
+        <p>
+          The Gup-E is a Gup with two separate cargo bays. Both cargo bay
+          windows can be opened to allow creatures and Octonauts to enter and
+          leave the Gup easily. It is Peso's preferred Gup, as it is often used
+          as an ambulance Gup.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>
+            Separate area in the back for sea creatures to be held and released.
+          </li>
+          <li>
+            The area in the back can be filled with water with the push of a
+            button for creatures that require water to breathe or stay healthy.
+          </li>
+          <li>Bait shooter, intended for deploying food or bait.</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup F</h3>
+        <img src="src/assets/images/Gup_f.JPG.webp" alt="Image of the Gup F" />
+        <p>
+          The Gup-F was a pedal-powered Gup, and the first one Tweak had ever
+          created. It ranks as the slowest-moving Gup.It was made to look like
+          an X-ray fish. It did not have any real interior or exterior, but a
+          basic metal frame with controls.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Pedal powered propeller</li>
+          <li>Basic stabilization fins</li>
+          <li>Headlamps and snackshooter are present but non-operational</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup G</h3>
+        <img src="src/assets/images/GupGToy-01.webp" alt="" />
+        <p>
+          This is a ship with sturdy legs for walking on ground and a special
+          tank for carrying large sea creatures. The GUP-G was built to travel
+          in water and on land, just like a salamander.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>
+            It is big enough to fit all of the Octonauts inside, including most
+            of their Gups.
+          </li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup H</h3>
+        <img src="src/assets/images/Gup_h.JPG.webp" alt="Gup H" />
+        <p>
+          The Gup H is built to look like a dragonfly. The "H" in its name
+          stands for helicopter, and it is used to soar above the sea and watch
+          the animals that hop in and out of the water
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Rotors use environmentally friendly oils for lubricant.</li>
+          <li>
+            Object shooter intended for shooting bait (so far mostly fish
+            biscuits).
+          </li>
+          <li>
+            Tether line that drops down through the bottom hatch for others to
+            grab onto.
+          </li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup I</h3>
+        <img src="src/assets/images/GUP_I.PNG.webp" alt="Image of the Gup I" />
+        <p>
+          The Gup I is a mobile ice research station built for freezing
+          conditions. According to Tweak, the "I" stands for ice. The design is
+          based on the tardigrade and is from Professor Natquik's old ice
+          research station which Tweak remodeled it into the Gup I.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Hot Cocoa Machine</li>
+          <li>Snow treads</li>
+          <li>Solar panels and wind turbines</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup J</h3>
+        <img src="src/assets/images/Gup_J.webp" alt="Image of the Gup J" />
+        <p>
+          The Gup-J is a relatively new Gup that most likely resembles a tree
+          frog.
+        </p>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup K</h3>
+        <img src="src/assets/images/Gup-K.webp" alt="Image of the Gup K" />
+        <p>
+          The Gup-K is half swamp boat and half tank. Barnacles drives the Gup-K
+          in The Great Swamp Search episode to save invasive species from
+          Tweak's home, the Everglades, from a wildfire. Its design is based off
+          of an alligator. Tweak had the idea for this Gup since she was a
+          little bunny.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>
+            This vehicle is one of the only amphibious gups so far, allowing it
+            to drive on both land and sea. A dome is included to help it go
+            underwater.
+          </li>
+          <li>
+            Mouth-shaped high pressure crushers mounted at the front of the
+            vehicle to clear debris.
+          </li>
+          <li>Two high-powered turbofans allow for water travel.</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup L</h3>
+        <img src="src/assets/images/Gup-L.webp" alt="" />
+        <p>The Gup L is a small hovercraft with one seat.</p>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup M</h3>
+        <img src="src/assets/images/Gup_m.webp" alt="Image of the Gup M" />
+        <p>
+          The Gup-M is a land-based Gup. It stands for the mudskipper and/or
+          motorcycle. It first appears as art in the Caves of Sac Actun movie
+          where Tweak, Codish, and Grouber use them to search the Yucatan Jungle
+          to find Barnacles and Peso Shellington's nephew Periwinkle even made a
+          toy based on this gup, which, according to his mother, makes a "lovely
+          back scratcher".
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Big wheels for driving through forests and mud.</li>
+          <li>Seats one crew member</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup N</h3>
+        <p>The Gup N is an upcoming Gup. Nothing is known about it yet,</p>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup O</h3>
+        <img
+          src="src/assets/images/Gup_O_Blueprints.webp"
+          alt="Blueprint image of the Gup O"
+        />
+        <p>
+          The Gup-O is an upcoming Gup. There is a slight glimpse of it on
+          Tweak's Octo-Tablet during The Great Swamp Search, as she scrolls
+          through the Gup deployment menu.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Skies on the front with snow treads on the rear</li>
+          <li>Based on an Orca</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup P</h3>
+        <img src="src/assets/images/gup-P.webp" alt="Gup P" />
+        <p>
+          The Gup-P is based off the parrot fish and is a Gup that cooperates
+          with the Gup-WThe Gup-P has an ambulance-like sound with an orange
+          light on the back. It is mostly heard while the Octonauts are driving
+          it.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Bubble engines</li>
+          <li>Siren and emergency light</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup Q</h3>
+        <img src="src/assets/images/gupQ.webp" alt="Gup Q" />
+        <p>
+          The Gup-Q is a Gup modeled after a Hammerhead Shark, and is designed
+          for ultimate search and rescue missions.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Built-in guide rope reel</li>
+          <li>Sonic-imaging echo mapper</li>
+          <li>Piranha Mode to chop away at rocks</li>
+          <li>
+            Gup-Q Scooters for searching in tighter, more narrow spaces
+            (underwater mode and land mode)
+          </li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup R</h3>
+        <img src="src/assets/images/Gup_R.webp" alt="Gup R" />
+        <p>
+          The Gup-R is a speedy Gup based on a Sailfish, the fastest fish in the
+          ocean. It is said to be the second fastest Gup of all, the Gup-B being
+          the first.
+        </p>
+        <h4>Features:</h4>
+        <ul>
+          <li>Turbo speed and hydro-jets</li>
+          <li>
+            Sail Fin (Calico Jack adds the pirate logo on it in Above and
+            Beyond) and Extending Legs when Active
+          </li>
+          <li>Nose - Grappling Hook</li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup S</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup T</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup U</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup V</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup W</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup X</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup Y</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="container">
+        {" "}
+        <h3>Gup Z</h3>
+        <img src="" alt="" />
+        <p></p>
+        <h4>Features:</h4>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </>
-  )
+  );
 }
