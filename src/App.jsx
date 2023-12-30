0b713857e58c9bea387dbc3081cfd1b5b5a29f3d
Dom <dom.benatar@hotmail.com>
2023-12-30 14:53:55 +0000

    adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
-import './App.css'
-import Home from "./components/pages/Home"
-import Team from "./components/pages/Team"
-import Gups from "./components/pages/Gups"
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/pages/Home";
+import Team from "./components/pages/Team";
+import Gups from "./components/pages/Gups";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <>
-    <header>
-      <h1>Maia's Octo database</h1>
-        <Link to="/">Home</Link>
-        <Link to="/team">Team</Link>
-        <Link to="/gups">Gups</Link>
-    </header>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/team" element={<Team />} />
-    <Route path="/gups" element={<Gups />} />
-    </Routes> 
+      <>
+        <div class="header">
+          <header>
+            <h1>
+              Maia's Octo database<a href="src/assets/images/logo.jpeg"></a>
+            </h1>
 
-    <footer><p>&copy;Created by Dom Benatar for Maia</p></footer>
-    </>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/team">Team</Link>
+                </li>
+                <li>
+                  <Link to="/gups">Gups</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/gups" element={<Gups />} />
+        </Routes>
+
+        <footer>
+          <p>&copy;Created by Dom Benatar for Maia</p>
+        </footer>
+      </>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
